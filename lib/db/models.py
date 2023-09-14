e9641448b6bb1aadb87416a70f7052c67be5e60a
Anandkumar Patel <avpatel2005@gmail.com>
2023-09-14 03:16:50 -0400

    create, test data base tables with alembic migrations

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,4 +1,15 @@
 from sqlalchemy.orm import declarative_base
-
-
+from sqlalchemy import Column, Integer, String
 Base = declarative_base() 
+
+class User(Base):
+    __tablename__= "users"
+
+    id = Column(Integer, primary_key=True)
+    
+
+class Contact(Base):
+    __tablename__= "contacts"
+
+    id = Column(Integer, primary_key=True)
+    
