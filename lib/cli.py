e863d6f1ef18898a1e00b960c9c2264ee789ae9f
Anandkumar Patel <avpatel2005@gmail.com>
2023-09-14 17:17:52 -0400

    In cli.py create start function and create clear_screen func

--- lib/cli.py
+++ lib/cli.py
@@ -1,14 +1,24 @@
 from db.models import Base, User, Contact
-from lib.db.hash import hash_password
+
+# from db.hash import hash_password
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from prettycli import Menu
+
+# from prettycli import Menu
 
 engine = create_engine("sqlite:///db/user_contacts.db")
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
 def start():
-    pass
+    clear_screen(40)
+    print("App start")
+
+
+def clear_screen(lines):
+    print("\n" * lines)
+
+
+start()
