add538da79374c934a6855740da1c7e44551f693
Anandkumar Patel <avpatel2005@gmail.com>
2023-09-14 04:02:29 -0400

    add final schema and create tables in db

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,15 +1,27 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String
-Base = declarative_base() 
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
+
 
 class User(Base):
-    __tablename__= "users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(15), nullable=False)
+    email = Column(String(50))
+    contacts = relationship("Contact", back_populates="user")
+
 
 class Contact(Base):
-    __tablename__= "contacts"
+    __tablename__ = "contacts"
 
     id = Column(Integer, primary_key=True)
-    
+    name = Column(String(50), nullable=False)
+    phone = Column(String(15))
+    email = Column(String(50))
+    category = Column(String, nullable=False)
+    address = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="contacts")
