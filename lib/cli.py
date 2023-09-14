da2ce9504b97c91917ac0ec97efb8ea1c5015e50
Anandkumar Patel <avpatel2005@gmail.com>
2023-09-14 17:35:50 -0400

    Download dependencies for simple term menu and set up landing page

--- lib/cli.py
+++ lib/cli.py
@@ -3,8 +3,8 @@ from db.models import Base, User, Contact
 # from db.hash import hash_password
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-# from prettycli import Menu
+from simple_term_menu import TerminalMenu
+from prettycli import red
 
 engine = create_engine("sqlite:///db/user_contacts.db")
 # Base.metadata.create_all(engine)
@@ -13,8 +13,12 @@ session = Session()
 
 
 def start():
-    clear_screen(40)
-    print("App start")
+    clear_screen(30)
+
+    print(red("Welcome to Contact storage App"))
+    options = ["Sign Up", "Login", "Exit"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
 
 
 def clear_screen(lines):
