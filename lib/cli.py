7b731f7e8f08c53b03bf8fd8c3870e005cd92f53
Anandkumar Patel <avpatel2005@gmail.com>
2023-09-14 17:45:07 -0400

    add funcionality to exit the app

--- lib/cli.py
+++ lib/cli.py
@@ -1,4 +1,6 @@
 from db.models import Base, User, Contact
+import time
+import sys
 
 # from db.hash import hash_password
 from sqlalchemy import create_engine
@@ -20,9 +22,30 @@ def start():
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
 
+    if options[menu_entry_index] == "Sign Up":
+        sign_up()
+    elif options[menu_entry_index] == "Login":
+        handle_login()
+    else:
+        exit()
+
 
 def clear_screen(lines):
     print("\n" * lines)
+
+
+def sign_up():
+    pass
+
+
+def handle_login():
+    pass
+
+
+def exit():
+    print("Exiting from app. Thanks for using Contact Storage App. Bye !!!")
+    time.sleep(2)
+    sys.exit()
 
 
 start()
